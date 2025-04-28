5b63e527cd4d747061a9cacd61b27edbde451a1f
jaygodara45IP <jay@infinitepossibilities.ai>
2025-04-29 04:40:03 +0530

    Added CORS headers manually - for vercel deployment

--- server.js
+++ server.js
@@ -14,6 +14,19 @@ connectDb()
 
 const app = express();
 app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+    res.setHeader('Access-Control-Allow-Credentials', 'true'); 
+
+    if (req.method === 'OPTIONS') {
+        return res.status(204).end(); 
+    }
+
+    next();
+});
+
 
 app.use(express.json());
 
